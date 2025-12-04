49639e2ed06a0b9f6e6dee2f4fae4515b9528cf9
RoneyThomas <roneythomas6@gmail.com>
2025-12-04 10:33:18 -0500

    Added host ip

--- app.py
+++ app.py
@@ -427,4 +427,4 @@ def delete_card(card_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Create database tables
-    app.run(debug=True)
+        app.run(host='0.0.0.0', port=5000)
